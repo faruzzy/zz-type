d5f3ce7bf3a1ec56d7919b4784c547360ab1038a
Roland Pangu <rolandpangu@hotmail.com>
2014-09-27 22:48:44 -0700

    Added ".jshintrc" file and $$ function

--- game.js
+++ game.js
@@ -35,6 +35,37 @@
 	check = false,
 	currentEnemy;
 
+	var $$ = function(selector, attributes) {
+		var tagRegex = /<([a-z]+)\/?/,
+			className = attributes.class,
+			textContent = attributes.text;
+
+		var matches = selector.match(tagRegex);
+		if ( matches && matches.length ) {
+			if ( !attributes ) {
+				return document.createElement(matches[1]);	
+			} else {
+				var el = document.createElement(matches[1]);
+
+				if ( className ) {
+					el.className = className;
+				} else if ( textContent ) {
+					el.textContent = textContent;
+				}
+
+				return el;
+			}
+		}
+
+		var elements = document.querySelectorAll(selector);
+		return elements.length === 1 ? elements.shift() : elements;
+	};
+
+	var el = $$('<div/>', {
+		class: 'test'
+	});
+
+
 	function randomIntFromInterval(min, max) {
 	    return Math.floor(Math.random() * (max-min + 1) + min);
 	}
