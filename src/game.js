9c2e121088d4ebd19b4653a10850fc21bfd5f1ea
Roland Pangu <faruzzy@gmail.com>
2016-01-13 18:25:00 -0700

    Added proto branch for solution using prototypes

--- src/game.js
+++ src/game.js
@@ -1,164 +1,89 @@
-(function( d ) {
-	'use strict';
+(function() {
+	function Game() {
+		this.KeyboardHash = (function() {
+			var alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
+			var map = {};
+			var start = 65;
+			for ( var i = 0; i < alphabet.length; i++ ) 
+				map[ alphabet[i] ] = start++;
+			return map;
+		})();
 
-	// we build a keyboard dictionary of all 
-	// the ascii characters with they corresponding
-	// keyCode values
-	const keyboardMap = (function() {
-		let map = {};
-		let alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
-		let start = 65;
-
-		for ( let i = 0; i < alphabet.length; i++ )
-			map[alphabet[i]] = start++;
-		return map;
-	})();
-
-	const dictionaryWords = d.split(/\s/).map(function( val ) { 
-		return val.toLowerCase(); 
-	});
-
-	let words = [],
-
-	point = 0,
-
-	wLength = dictionaryWords.length,
-
-	// is a word currently selected
-	inProgress = false,
-
-	/* The word that is currently selected */
-	currentEnemy,
-
-	/* The computed style of currentEnemy
-	 * that we get in order to not decrease its size 
-	 * has we shoot it
-	 */
-	ceOffsetWidth,
-
-	gameContainer = document.querySelector('.game');
-
-	/**
-	 * Returns a random number that is in the range
-	 * between a minimum and maximum value (all inclusive)
-	 * @param {Integer} A minimum value
-	 * @param {Integer} A maximum value
-	 * @return {Integer} A value that falls in the range provided
-	 */
-	function randomIntFromInterval( min, max ) {
-	    return Math.floor( Math.random() * (max-min + 1) + min );
-	}
-
-	/**
-	 * Returns the key corresponding to the value
-	 * of a dictionary
-	 * @method getKey
-	 * @param {Object} dictionary to look into
-	 * @param {Object} value we want the key of
-	 * @return {Object} key corresponding to the value
-	 */
-	Object.getKey = function( obj, value ) {
-		const keys = Object.keys( obj );
-		for ( const k in keys )
-			if ( obj[keys[k]] === value )
-				return keys[k];
-	};
-
-	/**
-	 * Display words to be typed on the page
-	 * @param {Array} array of strings
-	 */
-	function displayEnemies( words ) {
-		words.forEach( ( value ) => {
-			const s = 500;
-			const t = randomIntFromInterval(s, 4000);
-			setTimeout( () => {
-				let element = document.createElement('div');
-				element.className = 'enemy';
-				element.textContent = value;
-
-				gameContainer.appendChild(element);
-				setTimeout( () => {
-					element.classList.add('move');
-				}, s);
-			}, t);
+		this.dictionaryWords = d.split(/\s/).map(function(val) { 
+			return val.toLowerCase(); 
 		});
+
+		this.words = [];
+
+		this.wLength = dictionaryWords.length;
+
+		//this.gameContainer = $('.game');
+		this.gameContainer = document.querySelector('.game');
+
+		this.check = false;
+
+		this.currentEnemy = null;
 	}
-	
-	// select words to send to the player
-	(function() {
-		/**
-		 * Check if the string is already in the array
-		 * @param {String} to check for duplication
-		 * @return {Boolean} return true if the string is in the array
-		 */
-		function checkForDuplicate( str ) {
-			return words.some( ( word ) => {
-				return word === str;
-			});
-		}
 
-		for ( let i = 0, len = 5; i < len; i++ ) {
-			let idx, selectedWord;
+	Game.prototype = {
+		constructor: Game,
 
-			do {
-				idx = randomIntFromInterval(0, wLength);
-				selectedWord = dictionaryWords[idx];
-			} while ( checkForDuplicate(selectedWord) );
+		init: function() {
+			window.addEventListener('keydown', function(e) {
+				var letter = Object.getKey(this.keyboardHash, e.keyCode);
+				if (!check) {
+					$('.enemy:contains(' + letter + ')').each(function() {
+						if($(this).text().startsWith(letter)) {
+							currentEnemy =  $(this).addClass('selected');
 
-			words.push(selectedWord);
-		}
-		displayEnemies(words);
-	})();
+							var t = currentEnemy.text().slice(1);
+							currentEnemy.text(t);
+							check = true;
 
-	window.addEventListener('keydown', ( e ) => {
-		const letter = Object.getKey(keyboardMap, e.keyCode);
-		/**
-		 * Returns an array of all visible ememies,
-		 * that is which are not hidden since there's no point in looping over
-		 * those
-		 * @return {Array} of (words) node elements that are not hidden
-		 */
-		function getVisibleEnemies() {
-			let enemiesArr = Array.from(document.querySelectorAll('.selected'));
-			let visibleEnemies = enemiesArr.filter( ( element ) => {
-				if ( element.style.visibility !== 'hidden' ) 
-					return true;
-			});
-			return visibleEnemies;
-		}
+							return false;
+						}
+					});
+				}
 
-		// if the current state of the game 
-		// is such that no words is selected
-		if ( !inProgress )  {
-			inProgress = true;
-			const enemies = getVisibleEnemies().length ? 
-						  getVisibleEnemies() : 
-						  document.querySelectorAll('.enemy');
+		        if ( e.keyCode === this.keyboardHash[currentEnemy.text()[0]] ) {
+					var currentText = currentEnemy.text().slice(1);
+					currentEnemy.text(currentText);
 
-			Array.prototype.some.call(enemies, ( element ) => {
-				currentEnemy = element;
-				ceOffsetWidth = getComputedStyle(currentEnemy, null).getPropertyValue('width');
-				if ( element.textContent.startsWith(letter) ) {
-					if ( !currentEnemy.classList.contains('selected') ) {
-						currentEnemy.classList.add('selected');
+					if ( currentEnemy.text().length === 0 ) {
+						currentEnemy.hide();
+						check = false;
 					}
-					return true;
 				}
 			});
-		}
+		},
 
-		if ( currentEnemy.textContent.length > 0 ) {
-			const t = currentEnemy.textContent.slice(1);
-			currentEnemy.textContent = t;
-			currentEnemy.style.width = ceOffsetWidth;
-			point++;
-			if ( currentEnemy.textContent.length === 0 ) {
-				currentEnemy.style.visibility = 'hidden';
-				currentEnemy.classList.remove('selected');
-				currentEnemy.style.width = '0px';
-				inProgress = false;
+		create: function( words ) {
+			var self = this;
+			for (var i = 0, len = 5; i < len; i++) {
+				var idx, selectedWord;
+
+				do {
+					idx = randomIntFromInterval(0, self.wLength);
+					selectedWord = dictionaryWords[idx];
+				} while (words.some(function(val) { return val.startsWith(selectedWord[0]); }));
+
+				words.push(selectedWord);
 			}
+
+			words.forEach(function(value) {
+				var t = randomIntFromInterval(1000, 4000);
+				setTimeout(function(word) {
+					var tt = randomIntFromInterval(1000, 3000);
+					var element = document.createElement('div');
+					element.className = 'enemy';
+					element.textContent = word;
+					element.appendTo(gameContainer);
+
+					setTimeout(function(el) {
+						$('.game').find(el).addClass('move');
+					}, tt, element);
+				}, t, value);
+			});
 		}
-	});
-})( dictionary );
+	};
+})();
