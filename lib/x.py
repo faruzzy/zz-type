5b8a8fe5065389d4e6c066a9ebb67d4eeb29f4e8
Roland Pangu <faruzzy@gmail.com>
2016-01-14 23:30:16 -0700

    Begining to improve dictionary words mapping

---
+++ lib/x.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python
+'''
+I wrote this script to obtain words from the
+OS dictionary and format it properly
+'''
+def main():
+    from string import ascii_lowercase
+    letters = ascii_lowercase
+    xmap = {}
+    char = ''
+    # could be /usr/dict/words on other flavor
+    path = '/usr/share/dict/words'
+    with open(path) as f:
+        lines = [ line.strip().lower() for line in f.readlines() ]
+        char = ''
+        xlist = []
+        for line in lines:
+            if (len(char) == 0 and line[0] != char):
+                char = line[0]
+            if len(line) >= 3 and len(line) <= 12:
+                if (line.startswith(char)):
+                    xlist.append(line)
+                else:
+                    xmap[char] = xlist
+                    char = line[0]
+                    xlist = []
+    nmap = {}
+    with open('output.txt', 'a') as f:
+        for j in xrange(3, 13):
+            wlist = []
+            for char, words in xmap.items():
+                for word in words:
+                    if len(word) == j:
+                        wlist.append(word)
+            nmap[j] = wlist
+
+    with open('output.txt', 'a') as f:
+        for key, array in nmap.items():
+            f.write('{}: {},\n'.format(key, array))
+
+if __name__ == '__main__':
+    main()
